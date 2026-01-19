b640f384c772998f4979675a1aef336b70711f0d
Romslav <hun_a@inbox.ru>
2026-01-19 21:44:59 +0300

    fix: Add ErrorBoundary to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Toaster } from 'react-hot-toast'
 import AppRoutes from './router'
+import ErrorBoundary from './components/ErrorBoundary'
 
 const App: React.FC = () => {
   return (
-    <>
+    <ErrorBoundary>
       <AppRoutes />
       <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
-    </>
+    </ErrorBoundary>
   )
 }
 
