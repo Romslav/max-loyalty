c0d3871894e07518b1ba1ff77bd04521fcabf028
Romslav <hun_a@inbox.ru>
2026-01-20 21:01:25 +0300

    feat: Add barrel export for admin pages

---
+++ src/pages/admin/index.ts
@@ -0,0 +1,8 @@
+export { AuditLogs } from './AuditLogs';
+export { SupportTickets } from './SupportTickets';
+export { SystemSettings } from './SystemSettings';
+export { default as AdminDashboard } from './AdminDashboard';
+export { default as AnalyticsPage } from './AnalyticsPage';
+export { default as BillingManagement } from './BillingManagement';
+export { default as GuestsList } from './GuestsList';
+export { default as RestaurantsList } from './RestaurantsList';
