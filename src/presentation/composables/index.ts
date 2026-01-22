ee66164cd406cf4e07ead366df3cfb59e3c05bee
Romslav <hun_a@inbox.ru>
2026-01-23 02:51:52 +0300

    feat: update composables index with new helpers

--- src/presentation/composables/index.ts
+++ src/presentation/composables/index.ts
@@ -2,8 +2,13 @@
  * Composables Index - Reusable composition logic
  */
 
+// Core composables
 export { useAuth } from './useAuth';
 export { useUser } from './useUser';
 export { useGuest } from './useGuest';
 export { useFetch } from './useFetch';
+
+// Form composables
 export { useForm } from './useForm';
+export { useFormState } from './useFormState';
+export { useFormArray } from './useFormArray';
