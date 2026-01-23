38378ced06dea92bfed5fff58555d4c3dc394424
Romslav <hun_a@inbox.ru>
2026-01-23 13:44:40 +0300

    feat: Create presentation layer index

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,27 +1,5 @@
 /**
- * Presentation Layer - Vue Components + State Management
- * 
- * Этот лейер организует все Vue компоненты и управление состоянием
- * 
- * Архитектура:
- * - Components: Vue компоненты (Smart & Dumb)
- * - Stores: Pinia state management
- * - Composables: Переиспользуемая логика
- * - Views: Page компоненты
- * - Layouts: Layout компоненты
+ * Presentation Layer
  */
 
-// Components
-export * from './components';
-
-// Stores
-export * from './stores';
-
-// Composables
 export * from './composables';
-
-// Views
-export * from './views';
-
-// Layouts
-export * from './layouts';
