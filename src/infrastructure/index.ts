e2b10492a679e4ae396678491e4bae68891d6954
Romslav <hun_a@inbox.ru>
2026-01-23 13:28:43 +0300

    feat: Create infrastructure layer index

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,13 +1,6 @@
-// HTTP
-export { httpClient, HttpClient } from './http';
-export type { HttpClientConfig } from './http';
+/**
+ * Infrastructure Layer
+ */
 
-// DTOs
-export * from './dtos';
-
-// Repositories
-export { HttpUserRepository, HttpGuestRepository } from './repositories';
-
-// Dependency Injection
-export { container } from './di';
-export type { DIContainer } from './di';
+export * from './api';
+export * from './services';
