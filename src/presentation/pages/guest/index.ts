5ce1a0d955e752fdcacce42dff67e8683e496f04
Romslav <hun_a@inbox.ru>
2026-01-23 03:31:04 +0300

    feat: Create guest pages index with all exports

---
+++ src/presentation/pages/guest/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Guest Pages - Index
+ */
+
+export { default as LoginPage } from './LoginPage.vue';
+export { default as RegisterPage } from './RegisterPage.vue';
+export { default as ForgotPasswordPage } from './ForgotPasswordPage.vue';
+export { default as VerifyEmailPage } from './VerifyEmailPage.vue';
