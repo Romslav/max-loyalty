a7b6bb4b9de395fd20b959ac3a4949f1383f6a51
Romslav <hun_a@inbox.ru>
2026-01-23 03:08:22 +0300

    feat: Update common components index with new components

--- src/presentation/components/common/index.ts
+++ src/presentation/components/common/index.ts
@@ -1,9 +1,12 @@
 /**
- * Common Components - Reusable UI components
+ * Common Components - Index
  */
 
-export { default as AppButton } from './AppButton.vue';
-export { default as AppInput } from './AppInput.vue';
-export { default as AppCard } from './AppCard.vue';
-export { default as AppLoader } from './AppLoader.vue';
 export { default as AppAlert } from './AppAlert.vue';
+export { default as AppButton } from './AppButton.vue';
+export { default as AppCard } from './AppCard.vue';
+export { default as AppInput } from './AppInput.vue';
+export { default as AppLoader } from './AppLoader.vue';
+export { default as AppTabs } from './AppTabs.vue';
+export { default as AppTab } from './AppTab.vue';
+export { default as AppModal } from './AppModal.vue';
