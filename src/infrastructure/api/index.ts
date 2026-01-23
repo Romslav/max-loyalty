5f25cea10a69f74be85f96cb5e8fc25b3b9eada9
Romslav <hun_a@inbox.ru>
2026-01-23 13:28:30 +0300

    feat: Create API layer index

---
+++ src/infrastructure/api/index.ts
@@ -0,0 +1,7 @@
+/**
+ * API Layer
+ */
+
+export { httpClient } from './http-client';
+export { apiConfig, endpoints, HttpStatus } from './config';
+export type { ApiResponse } from './http-client';
