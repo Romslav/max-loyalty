3a1e348f5ac8f02610830b2b9a4c8bd70b9c46ba
Romslav <hun_a@inbox.ru>
2026-01-23 03:58:09 +0300

    feat: Create main pages index with all exports

---
+++ src/presentation/pages/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Все страницы приложения - Центральный каталог
+ */
+
+// Guest Pages
+export * from './guest';
+
+// Core Pages
+export * from './core';
+
+// User Pages
+export * from './user';
