f34d2af32ff526eac2c8116c754e5002db257164
Romslav <hun_a@inbox.ru>
2026-01-23 03:04:13 +0300

    feat: Add user components index file

---
+++ src/presentation/components/user/index.ts
@@ -0,0 +1,6 @@
+/**
+ * User Components - Index
+ */
+
+export { default as UserPreferencesDisplay } from './UserPreferencesDisplay.vue';
+export { default as SecuritySettings } from './SecuritySettings.vue';
