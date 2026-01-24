59e45e36c951bd9b0c36aa5a13e6097b879e3a73
Romslav <hun_a@inbox.ru>
2026-01-25 01:01:19 +0300

    feat: add export index for promotion components

---
+++ src/ui/components/promotion/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Promotion Components Index
+ * 
+ * Central export point for all promotion-related components.
+ */
+
+export { PromoCodeValidator } from './PromoCodeValidator'
+export type { PromoCode } from './PromoCodeCard'
+export { PromoCodeCard } from './PromoCodeCard'
+export { PromotionForm } from './PromotionForm'
+export type { PromotionFormData } from './PromotionForm'
