3aea6a1b133c254e1ed7e2b71e2d56fe326051ca
Romslav <hun_a@inbox.ru>
2026-01-24 17:15:39 +0300

    refactor: add HttpPointsRepository to repositories exports

--- src/infrastructure/repositories/index.ts
+++ src/infrastructure/repositories/index.ts
@@ -1,2 +1,4 @@
 export { HttpUserRepository } from './HttpUserRepository';
 export { HttpGuestRepository } from './HttpGuestRepository';
+export { HttpPointsRepository } from './http/HttpPointsRepository';
+export type { PointsOperationDTO, PointsBalanceDTO, PointsHistoryResponse } from './http/HttpPointsRepository';
