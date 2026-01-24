b4cd6309713a9daf5838a8a36c53427bed5180e3
Romslav <hun_a@inbox.ru>
2026-01-24 17:20:13 +0300

    refactor: add points components export index

---
+++ src/components/points/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Points Components
+ * 
+ * Barrel export for all points-related components
+ */
+
+export { PointsBalanceCard } from './PointsBalanceCard'
+export type { PointsBalanceCardProps } from './PointsBalanceCard'
+
+export { PointsOperationForm } from './PointsOperationForm'
+export type { PointsOperationFormProps } from './PointsOperationForm'
+
+export { PointsHistoryList } from './PointsHistoryList'
+export type { PointsHistoryListProps } from './PointsHistoryList'
+
+export { PointsStatsPanel } from './PointsStatsPanel'
+export type { PointsStatsPanelProps } from './PointsStatsPanel'
+
+export { PointsPageContainer } from './PointsPageContainer'
+export type { PointsPageContainerProps } from './PointsPageContainer'
