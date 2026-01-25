18e2faaf5bc4e0d10ec60a6a40f1e902f8faf541
Romslav <hun_a@inbox.ru>
2026-01-25 20:34:38 +0300

    fix: Create services index file with correct exports

---
+++ backend/src/infrastructure/services/index.ts
@@ -0,0 +1,4 @@
+export { GuestServiceImpl } from './GuestServiceImpl';
+export { TransactionServiceImpl } from './TransactionServiceImpl';
+export { CardServiceImpl } from './CardServiceImpl';
+export { RestaurantServiceImpl } from './RestaurantServiceImpl';
