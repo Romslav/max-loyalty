ebbba04ecbd3a90cfb9dfce95860b3d0eda25616
Romslav <hun_a@inbox.ru>
2026-01-25 20:29:59 +0300

    fix: Create all missing domain service interfaces

---
+++ backend/src/domain/services/index.ts
@@ -0,0 +1,4 @@
+export { IGuestService, IGuest, IVerificationResult } from './GuestService';
+export { ITransactionService } from './TransactionService';
+export { ICardService } from './CardService';
+export { IRestaurantService } from './RestaurantService';
